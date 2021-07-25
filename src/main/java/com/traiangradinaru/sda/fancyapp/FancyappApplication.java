5050f03fe83ed65af94d0c83ec5c2ace75a07dc9
Traian Gradinaru <traian.gradinaru@gmail.com>
2021-07-25 12:55:25 +0300

    Add initial structure for Fancy project

---
+++ src/main/java/com/traiangradinaru/sda/fancyapp/FancyappApplication.java
@@ -0,0 +1,13 @@
+package com.traiangradinaru.sda.fancyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traiangradinaru/sda/fancyapp/FancyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traiangradinaru.sda.fancyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
